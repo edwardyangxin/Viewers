58487f0da1f93836034f535cdbfe601d044637d5
edward <yx_yoyo@live.cn>
2023-11-07 16:39:03 -0800

    evibased: 中文 header

--- platform/app/src/routes/WorkList/WorkList.tsx
+++ platform/app/src/routes/WorkList/WorkList.tsx
@@ -433,16 +433,17 @@ function WorkList({
   const commitHash = process.env.COMMIT_HASH;
 
   const menuOptions = [
-    {
-      title: t('Header:About'),
-      icon: 'info',
-      onClick: () =>
-        show({
-          content: AboutModal,
-          title: t('AboutModal:About OHIF Viewer'),
-          contentProps: { versionNumber, commitHash },
-        }),
-    },
+    // evibased, add evibased about? or just remove it.
+    // {
+    //   title: t('Header:About'),
+    //   icon: 'info',
+    //   onClick: () =>
+    //     show({
+    //       content: AboutModal,
+    //       title: t('AboutModal:About OHIF Viewer'),
+    //       contentProps: { versionNumber, commitHash },
+    //     }),
+    // },
     {
       title: t('Header:Preferences'),
       icon: 'settings',
