feae29a6e4412ef1cb1c02294949a11a65373dd0
edward <yx_yoyo@live.cn>
2023-11-10 16:36:38 -0800

    evibased:

--- platform/ui/src/components/TimePointSummary/TimepointSummary.tsx
+++ platform/ui/src/components/TimePointSummary/TimepointSummary.tsx
@@ -5,7 +5,7 @@ const TimePointSummary = ({ timepoint, modality, description }) => {
   return (
     <div className="p-2">
       <div className="leading-none">
-        <span className="mr-2 text-base font-bold text-white">{'正在标注: ' + timepoint}</span>
+        <span className="mr-2 text-lg font-bold text-white">{'正在标注: ' + timepoint}</span>
         <span className="bg-common-bright rounded-sm px-1 text-base font-bold text-black">
           {modality}
         </span>
