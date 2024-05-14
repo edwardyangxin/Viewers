a2b74d5b47b99fc7593de6ddf51dae6b34a65054
edward <yx_yoyo@live.cn>
2024-05-14 15:10:55 -0700

    evibased: fix bug

--- extensions/cornerstone/src/getToolbarModule.tsx
+++ extensions/cornerstone/src/getToolbarModule.tsx
@@ -250,7 +250,7 @@ export default function getToolbarModule({ commandsManager, servicesManager }) {
         const displaySets = displaySetUIDs.map(displaySetService.getDisplaySetByUID);
 
         const areReconstructable = displaySets.every(displaySet => {
-          return displaySet.isReconstructable;
+          return displaySet?.isReconstructable; // evibased, displaySet could be undefined
         });
 
         if (!areReconstructable) {
